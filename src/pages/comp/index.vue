9ac67987af213aa2ba41fecae1202bdb4c6cf555
张腾跃 <tengyuezhang@91jkys.com>
2020-10-16 11:22:31 +0800

    init

---
+++ src/pages/comp/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>自定组件</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ src/pages/other/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>其他事项</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
